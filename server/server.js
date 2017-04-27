50ef15cc4fbf14b541ac203ef1cbb4b6827c49e6
Gil Tayar <gil@tayar.org>
2017-04-27 14:59:40 +0300

    now also serves the webpack assets

--- server/server.js
+++ server/server.js
@@ -126,6 +126,7 @@ app.post('/add-new-playlist-with-song', (req, res) => {
 
 const path = require('path');
 app.get('/app.js', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/app.js')));
+app.use('/_', express.static(path.resolve(__dirname, '../dist/_')));
 app.get('/**', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
 
 // START THE SERVER
